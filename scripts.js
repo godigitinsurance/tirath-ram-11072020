36d964134d0ccf147f5d6a64ec2de1ceddba7ea7
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-10 14:29:19 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Tirath Ram");
+$("#policyNumber").text("RE46202721");
+$("#startDate").text("11-Jul-2020");
+$("#endDate").text("10-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CK3326");
+$("#make").text("HERO MOTOCORP");
+$("#model").text("PASSION PRO");
+$("#vehicalSubType").text("SELF-START");
+$("#engineNo").text("HA10ENDHD57791");
+$("#chassis").text("MBLHA10AWDHD33144");
